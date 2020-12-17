b61dbd7b06ade5fa3f7b894411022beba51f303f
Aymeric Anger <daedelus.git@gmail.com>
2020-12-17 16:39:06 +0100

    Base

--- public/index.php
+++ public/index.php
@@ -1,2 +1,8 @@
 <?php
 
+// Slim App Loader
+$root = dirname(__DIR__);
+
+require_once "$root/vendor/autoload.php";
+
+if (isset($app) and $app instanceof \Slim\App) $app->run();
--- src/app.php
+++ src/app.php
@@ -1,2 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
+use Dotenv\Dotenv;
+
+@define('APP_ROOT', dirname(__DIR__));
+@define('APP_BIN', dirname(__DIR__) . "/bin");
+@define('APP_CONFIG', dirname(__DIR__) . "/config");
+@define('APP_DOCS', dirname(__DIR__) . "/docs");
+@define('APP_LOGS', dirname(__DIR__) . "/logs");
+@define('APP_PUBLIC', dirname(__DIR__) . "/public");
+@define('APP_RESOURCES', dirname(__DIR__) . "/resources");
+@define('APP_SRC', dirname(__DIR__) . "/src");
+@define('APP_TEMPLATES', dirname(__DIR__) . "/templates");
+@define('APP_TESTS', dirname(__DIR__) . "/tests");
+@define('APP_TMP', dirname(__DIR__) . "/tmp");
+
+
+(Dotenv::createUnsafeImmutable(APP_ROOT))->load();
+
+
